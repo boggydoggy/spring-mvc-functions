8bf4c610a7fdface3feb210a204ef8a189d0dba4
boggydoggy <zhd401@naver.com>
2025-02-11 13:07:09 +0900

    Initial Commit, Inflearn Spring MVC part 1.8 Spring MVC Basic
    - Added Mapping annotation examples

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -3,8 +3,7 @@ package hello.springmvc.basic;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //@Slf4j
 @RestController
@@ -23,6 +22,48 @@ public class LogTestController {
         logger.warn("warn log = {}", name);
         logger.error("error log = {}", name);
 
+        return "done";
+    }
+
+    @GetMapping("/mapping/{userId}")
+//    public String mappingPath(@PathVariable String userId) {
+    public String mappingPath(@PathVariable("userId") String data) {
+        logger.info("MappingPath userId = {}", data);
+
+        return "done";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{order}")
+    public String mappingPath2(@PathVariable String userId, @PathVariable String order) {
+        logger.info("MappingPath userId = {}, order = {}", userId, order);
+
+        return "done";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        logger.info("mappingConsumes");
+        return "done";
+    }
+
+    /**
+     * Accept 헤더 기반 Media Type
+     * produces = "text/html"
+     * produces = "!text/html"
+     * produces = "text/*"
+     * produces = "*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces = "text/html")
+    public String mappingProduces() {
+        logger.info("mappingProduces");
         return "done";
     }
 }
